714b49a50703153d12595f4ba6e5808d1f9fdc47
simrann 02 <simran292002@gmail.com>
2025-05-17 23:43:47 +0530

    Starting out with java

---
+++ First/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        System.out.println(~3);
+    }
+}
